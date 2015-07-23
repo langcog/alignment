997fb606143a45bd2bdafa20814280859807ad96
Jake Prasad <jjprasad@ucsd.edu>
2015-07-23 11:48:12 -0700

    Resorted header

--- twtr.py
+++ twtr.py
@@ -111,7 +111,7 @@ def readCSV(inputFile, users, numOfMarkers):
 	markers = []
 	freqs = [(k, freqs[k]) for k in sorted(freqs, key=freqs.get, reverse=True)]
 	subset = freqs[0:numOfMarkers]
-
+	
 	for subsetTuple in subset:
 		logger.log(subsetTuple)
 		if(subsetTuple[0] == "[mention]" or subsetTuple[0] == "[url]"):
